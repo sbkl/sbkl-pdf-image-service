030b16dab8876d6aa88e97419c84bc372d61cb3f
sbkl <sebastien@sbkl.ltd>
2026-02-15 10:51:31 +0800

    alt7

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
-import { processDocumentImagesRouter } from "./routes/processDocumentImages";
+// import { processDocumentImagesRouter } from "./routes/processDocumentImages";
 import { processDocumentImagesRouterV2 } from "./routes/process-document-images";
 
 const app = new Hono();
@@ -8,7 +8,7 @@ const app = new Hono();
 app.use("*", logger());
 
 app.get("/health", (c) => c.json({ status: "ok" }, 200));
-app.route("/v1", processDocumentImagesRouter);
+// app.route("/v1", processDocumentImagesRouter);
 
 app.route("/v2", processDocumentImagesRouterV2);
 export { app };
